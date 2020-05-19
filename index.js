7b7ac5b84d7fbfd16ad2e5a6fb299f94b9d6ce61
Constantin <razvan1598@gmail.com>
2020-05-19 15:03:49 +0200

    added html functionalities

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// regex for only number from 0 to 9:
+// document.getElementById("").addEventListener("input",  (e) => {
+// const numbers = /^[0-9]+$/;
+// if(e.target.value < 0 || !e.target.value.match(numbers)) {
+//     e.target.value = 0;
+// } else if (e.target.value > 8 || e.target.value.toString().length > 1){
+//     e.target.value = 8;
+// }
