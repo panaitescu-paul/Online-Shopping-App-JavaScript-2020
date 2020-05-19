b4fbeabd232c045ebfc9d29848f6d13f2c7894a2
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-05-19 11:11:10 +0200

    test.js - structure

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 // Order of execution:
+// ls / dir
+// cd to folder
 // npm i
 // to run just the tests: npm test
 // to test code coverage: npm run coverage
@@ -9,3 +11,14 @@ let expect = chai.expect;
 let assert = require('chai').assert;
 
 chai.should();
+
+// let Purchase = require("../testing_ma1/purchase");
+
+describe('test', () => {
+    describe('test', () => {
+        beforeEach(() => {
+        });
+        describe('1', () => {
+        });
+    });
+});
