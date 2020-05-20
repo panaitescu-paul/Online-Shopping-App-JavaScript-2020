19ca46ba14bbccf0e9181ca2f910fc02da4d68f9
MariusMM2 <dmunteanu91@gmail.com>
2020-05-20 13:12:25 +0200

    added product and cartproduct classes

---
+++ purchase.js
@@ -0,0 +1,15 @@
+class CartProduct {
+    id;
+    quantity;
+
+    constructor(id, quantity) {
+        this.id = id;
+        this.quantity = quantity;
+    }
+}
+
+class Product {
+    id;
+    name;
+    price;
+}
