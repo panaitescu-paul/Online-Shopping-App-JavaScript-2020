d599efbfcf385012ea5f5c62bfc5ef4fdba98176
Constantin <razvan1598@gmail.com>
2020-05-20 13:43:27 +0200

    separate configuration from implementation

--- purchase.js
+++ purchase.js
@@ -13,3 +13,18 @@ class Product {
     name;
     price;
 }
+
+function loadJSON(fileName) {
+    let xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', fileName, false); // Replace 'products' with the path to your file
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState === 4 && xobj.status === 200) {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            // callback(xobj.responseText);
+            return xobj.responseText;
+        }
+    };
+    xobj.send(null);
+    return JSON.parse(xobj.responseText);
+}
