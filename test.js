ea4d06c7287f241f7bfd68c4266235e42a398c98
Constantin <razvan1598@gmail.com>
2020-05-23 14:05:56 +0200

    implemented the structure for tests added describes for each setter

--- test.js
+++ test.js
@@ -17,14 +17,13 @@ const loadJsonFile = require('load-json-file');
 let Purchase = require("../ma2-testing-project/purchase");
 
 describe('Purchase', () => {
-    describe('Name of the functionality', () => {
+    describe('Product quantity', () => {
         let purchase;
         let PRODUCTS
 
         beforeEach(async () => {
             purchase = new Purchase();
             PRODUCTS = await loadJsonFile("products.json");
-
         });
 
         describe('', () => {
@@ -48,6 +47,123 @@ describe('Purchase', () => {
                 // purchase.internetConnection(true);
                 // purchase.isInternetConnection.should.equal(true);
             });
+        });
+    });
+    describe('First name', () => {
+        let purchase;
+        let PRODUCTS
+
+        beforeEach(async () => {
+            purchase = new Purchase();
+            PRODUCTS = await loadJsonFile("products.json");
+        });
+
+        describe('', () => {
+
+        });
+    });
+    describe('Last name', () => {
+        let purchase;
+        let PRODUCTS
+
+        beforeEach(async () => {
+            purchase = new Purchase();
+            PRODUCTS = await loadJsonFile("products.json");
+        });
+
+        describe('', () => {
+
+        });
+    });
+    describe('Age', () => {
+        let purchase;
+        let PRODUCTS
+
+        beforeEach(async () => {
+            purchase = new Purchase();
+            PRODUCTS = await loadJsonFile("products.json");
+        });
+
+        describe('', () => {
+
+        });
+    });
+    describe('Email', () => {
+        let purchase;
+        let PRODUCTS
+
+        beforeEach(async () => {
+            purchase = new Purchase();
+            PRODUCTS = await loadJsonFile("products.json");
+        });
+
+        describe('', () => {
+
+        });
+    });
+    describe('Address', () => {
+        let purchase;
+        let PRODUCTS
+
+        beforeEach(async () => {
+            purchase = new Purchase();
+            PRODUCTS = await loadJsonFile("products.json");
+        });
+
+        describe('', () => {
+
+        });
+    });
+    describe('Card number', () => {
+        let purchase;
+        let PRODUCTS
+
+        beforeEach(async () => {
+            purchase = new Purchase();
+            PRODUCTS = await loadJsonFile("products.json");
+        });
+
+        describe('', () => {
+
+        });
+    });
+    describe('Card security code', () => {
+        let purchase;
+        let PRODUCTS
+
+        beforeEach(async () => {
+            purchase = new Purchase();
+            PRODUCTS = await loadJsonFile("products.json");
+        });
+
+        describe('', () => {
+
+        });
+    });
+    describe('Delivery option', () => {
+        let purchase;
+        let PRODUCTS
+
+        beforeEach(async () => {
+            purchase = new Purchase();
+            PRODUCTS = await loadJsonFile("products.json");
+        });
+
+        describe('', () => {
+
+        });
+    });
+    describe('Buy products', () => {
+        let purchase;
+        let PRODUCTS
+
+        beforeEach(async () => {
+            purchase = new Purchase();
+            PRODUCTS = await loadJsonFile("products.json");
+        });
+
+        describe('', () => {
+
         });
     });
 });
