7916d061f0f1c36d95ef92cb68e298fe1e5f6294
Constantin <razvan1598@gmail.com>
2020-05-27 12:51:25 +0200

    changed npm i to install on order of execution

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // Order of execution:
 // ls / dir
 // cd to folder
-// to install the dependencies run: npm i
+// to install the dependencies run: npm install
 // to run just the tests: npm test
 // to test code coverage: npm run coverage
 let mocha = require('mocha');
